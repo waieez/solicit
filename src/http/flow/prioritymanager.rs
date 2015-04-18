bd51ae88f191d4f2856c314192da7cf3a1be3500
Eli Xian <elixian@gmail.com>
2015-04-18 15:32:19 -0500

    feat(flow) scaffold priority manager, stream priority

--- src/http/flow/prioritymanager.rs
+++ src/http/flow/prioritymanager.rs
@@ -7,7 +7,77 @@ pub struct StreamPriority {
     children: Vec<u32>
 }
 
+impl StreamPriority {
+
+    // creates an node
+    fn new () {
+
+    }
+
+    // sets the parent of a given stream
+    fn set_parent () {
+
+    }
+
+    // adds a child to the given stream
+    fn add_child () {
+
+    }
+
+    // sets the current stream's siblings as children of itself
+    fn set_exclusive () {
+
+    }
+}
+
 pub struct PriorityManager {
     streams: HashMap<u32, StreamPriority>,
-    root: VecDeque<u32>
+    queue: VecDeque<u32>
+}
+
+
+impl PriorityManager {
+    // creates an instance of a priority manager for the connection
+    pub fn new () {
+
+    }
+
+    // gets the first independant stream id from the queue
+    pub fn get () {
+
+    }
+
+    // pushes stream id onto queue
+    pub fn add () {
+
+    }
+
+    // adds stream id to hashmap with parent pointer set
+    pub fn add_with_dependancy () {
+
+    }
+
+    // modifies a given's stream's dependancy
+    pub fn set_as_dependancy () {
+
+    }
+
+    // set's a given stream as an exclusive dependancy
+    pub fn set_as_exclusive () {
+
+    }
+
+    // removes the stream id and all parent/child associations from priority manager
+    // forces stream's parent (if any) to adopt its children (if any)
+    pub fn retire () {
+
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::{PriorityManager, StreamPriority};
+
+    
 }
